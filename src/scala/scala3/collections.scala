fc9face76286ee3edebd5a9212933942964f8884
ValentinShilin <valentin.shilin.de@gmail.com>
2024-11-26 20:24:31 +0100

    scala 2

--- src/scala/scala3/collections.scala
+++ src/scala/scala3/collections.scala
@@ -1,3 +1,31 @@
 package scala.scala3
 
 import scala.collection.immutable
+
+object collections {
+  def main(args: Array[String]): Unit = {
+    val democollection1 = "line 1" :: "line 2" :: "line 3" :: Nil
+    val democollection2 = List("line 1", "line 2", "line 3")
+    val demoSet = ("line 1" :: "line 2" :: "line 2" :: "line 3" :: Nil).toSet
+    demoSet.foreach(x => println(x))
+
+    ("line 1" :: "line 2" :: "line 2" :: "line 3" :: Nil).groupBy(x=>x).map(x=>x._1).foreach(x => println(x))
+
+    case class testcaseclass(x: String, y: Int)
+    val test1 = testcaseclass("1", 1)
+    val test2 = testcaseclass("2", 2)
+    val test3 = testcaseclass("3", 2)
+    val test4 = testcaseclass("4", 2)
+    println("---------------")
+    (test1 :: test2 :: test3 :: test4 :: Nil).groupBy(x => x.y).map(x => x._2.head).foreach(u => println(u))
+    println("---------------")
+
+    val test = List(1,2,3,4,5) :: List(1,50,3) :: List(1,2) :: Nil
+    test.filter(x=>x.sum > 10).foreach(x=>println(x.mkString(",")))
+
+
+
+
+
+  }
+}
--- src/scala/scala3/forcomp.scala
+++ src/scala/scala3/forcomp.scala
@@ -1,1 +1,62 @@
 package scala.scala3
+
+object forcompr {
+  def main(args: Array[String]): Unit = {
+    val RGB = Seq("R", "G", "B")
+    val range = Range(1,4)
+    val map = Map("R"->"Red", "G"->"Gruene", "B"->"Blau")
+    println("----------")
+    for (el <- RGB){
+      println(el)
+    }
+
+    println("----------")
+    for (el <- RGB; el1 <- range) {
+      println(s"$el, $el1")
+    }
+    println("----------")
+    for (
+      el1 <- RGB;
+      el2 <- RGB;
+      el3 <- RGB;
+      if el1 !=el2;
+      if el3!=el2 && el3!=el1
+    ) {
+      println(s"$el1 $el2 $el3")
+    }
+
+    println("example")
+    case class Car(marke: String, model: String, year: Int)
+    val cars = Car("VW", "Passat", 2005) :: Car("Lexus", "Ux", 2019) :: Car("BMW", "ix4", 2023) :: Nil
+    case class Garage(name: String, index: Int)
+    val garages = Garage("BMW", 1) :: Garage("Lexus", 2) :: Nil
+    garages.flatMap{
+      garage => cars.filter(car => car.marke == garage.name).map(car => (car.marke, car.model, garage.index))
+    }.foreach{case (m,g,i)=> println(s"$m $g $i")}
+
+    println("for compr.")
+
+    val cars2000 = for {
+      car <- cars if car.year >2010
+    } yield (car.marke, car.model)
+    cars2000.foreach(x=>println(s"${x._1} ${x._2}"))
+
+
+    println("----------------------")
+
+    val car1 = for {
+      car <- cars
+      garage <- garages
+      if car.marke == garage.name
+    } yield {
+      (car.marke, car.model, garage.index)
+    }
+
+    car1.foreach({
+      case(marke, model, index) => println(s"$marke, $model, $index")
+    })
+
+
+
+  }
+}
